83d00db572154b8bba5487b332876cce034bd9d6
ege-ozgur <egeozgur240@gmail.com>
2026-05-27 20:54:44 +0100

    Initial commit: UE5.4.4 Third Person C++ template

---
+++ Source/PhoenixAscendant/PhoenixAscendant.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "PhoenixAscendant.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PhoenixAscendant, "PhoenixAscendant" );
+ 
---
+++ Source/PhoenixAscendant/PhoenixAscendantGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PhoenixAscendantGameMode.generated.h"
+
+UCLASS(minimalapi)
+class APhoenixAscendantGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	APhoenixAscendantGameMode();
+};
+
+
+
